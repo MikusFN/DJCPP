e54f58829cb6b22a26d764936cd79be5df4673c2
fernando.nogueira@mydidimo.com <fernando.nogueira@mydidimo.com>
2021-03-09 01:02:46 +0000

    more assetd for the obstacles

--- PreliminaryProjectDJC/Assets/Scripts/PlayerController.cs
+++ PreliminaryProjectDJC/Assets/Scripts/PlayerController.cs
@@ -32,9 +32,19 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    public void Move(float move, bool axis, Vector2 mousePos)
+    public void Move(float move, bool axis, float torqueValue)
     {
-        Vector3 targetVelocity = m_Velocity;        
+        Vector3 targetVelocity = m_Velocity;
+
+        //Quaternion quat = Quaternion.LookRotation(m_Rigidbody2D.transform.up, m_Rigidbody2D.transform.forward);
+        //quat.z += mousePos.x;
+
+        //Vector2 lookDir = (mousePos - m_Rigidbody2D.position).normalized;
+
+        //m_Rigidbody2D.transform.Rotate(new Vector3(0, 0, Mathf.Lerp(-90, 90,1)));
+
+        //m_Rigidbody2D.AddForceAtPosition(transform.right.normalized * torqueValue*0.03f, 
+        //    m_Rigidbody2D.position - new Vector2(m_Rigidbody2D.position.x, -GetComponent<SpriteRenderer>().bounds.size.y));
 
         if (axis)
         {
@@ -51,9 +61,9 @@ public class PlayerController : MonoBehaviour
         // And then smoothing it out and applying it to the character
         m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
-        Vector2 lookDir = (mousePos - m_Rigidbody2D.position)*2.0f;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        m_Rigidbody2D.rotation = angle;
+        //Vector2 lookDir = (mousePos - m_Rigidbody2D.position);
+        //float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        //m_Rigidbody2D.rotation = angle;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
--- PreliminaryProjectDJC/Assets/Scripts/PlayerMovement.cs
+++ PreliminaryProjectDJC/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
 
     float horizontalMove = 0f;
     float verticalMove = 0f;
+    float torqueValue = 0f;
     Vector2 mousePos = Vector2.zero;
 
     // Start is called before the first frame update
@@ -24,12 +25,19 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * multSpeed;
         verticalMove = Input.GetAxisRaw("Vertical") * multSpeed;
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            torqueValue = horizontalMove/multSpeed;
+            horizontalMove = 0.0f;
+        }
+        //mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, true, mousePos);
-        controller.Move(verticalMove * Time.fixedDeltaTime, false, mousePos);
+        //controller.Move(horizontalMove * Time.fixedDeltaTime, true, mousePos);
+        //controller.Move(verticalMove * Time.fixedDeltaTime, false, mousePos);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, true, torqueValue);
+        controller.Move(verticalMove * Time.fixedDeltaTime, false, torqueValue);
     }
 }
