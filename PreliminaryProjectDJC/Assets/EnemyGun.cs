3bedc7220a16e29a97b113b14001f45aec34f2da
danicm47 <56267616+danicm47@users.noreply.github.com>
2021-03-17 00:14:30 +0000

    Bullets bug

--- PreliminaryProjectDJC/Assets/EnemyGun.cs
+++ PreliminaryProjectDJC/Assets/EnemyGun.cs
@@ -9,12 +9,12 @@ public class EnemyGun : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Invoke ("FireEnemyBullet", 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Invoke ("FireEnemyBullet", 1f);
     }
 
     void FireEnemyBullet() {
